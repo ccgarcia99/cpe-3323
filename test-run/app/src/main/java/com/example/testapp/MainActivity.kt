e13612c760cb56d423c147adaa721a27b1dd108b
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-06-17 20:16:39 +0800

    Created basic UI elements on 'Test App'

--- test-run/app/src/main/java/com/example/testapp/MainActivity.kt
+++ test-run/app/src/main/java/com/example/testapp/MainActivity.kt
@@ -3,58 +3,65 @@ package com.example.testapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.compose.material3.Button
+import androidx.compose.ui.unit.dp
 import com.example.testapp.ui.theme.TestAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TestAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
-        MyCustomText(str = "Hello, $name!")
-        MyCustomSubHeading(str = "This is a test!")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("Franthy", "Uriel")
+) {
+    Column(modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
-@Preview(showBackground = true)
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(25.dp)) {
+            Text(text = "Hello, $name!")
+        }
+    }
+}
+
+// Preview toolkit.
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     TestAppTheme {
-        Greeting("Android")
+        MyApp()
     }
-}
-
-@Composable
-fun MyCustomText(str: String) {
-    return Text(text = str, fontSize = 50.sp, lineHeight = 120.sp)
-}
-
-@Composable
-fun MyCustomSubHeading(str: String) {
-    return Text(text = str, fontSize = 20.sp, lineHeight = 50.sp)
 }
