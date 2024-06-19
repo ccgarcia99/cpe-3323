81a5b9cef61adaecc1a2702a40bb57e3f8680b2a
Christian Clyde G. Decierdo <decierdocc1999@gmail.com>
2024-06-19 09:40:23 +0800

    CREATED exercise 1

--- kotlin-basics/kotlin-demo/src/Main.kt
+++
@@ -1,8 +0,0 @@
-class Main() {
-    
-    fun main() {
-
-    }
-
-}
-
---
+++ kotlin-basics/kotlin-exercises/exercise-1/team2_exercise-1-attendees/src/team2_exercise-1-attendees.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val attendees = mutableListOf<String>()
+    println("Enter the names of attendees, one at a time. Enter an empty line to finish:")
+
+    var count = 0
+    while (count < 10) {
+        print("Enter name ${count + 1}: ")
+        val input = readlnOrNull()
+
+        if (input.isNullOrEmpty()) {
+            break
+        } else {
+            attendees.add(input)
+            count++
+        }
+    }
+
+    // Sort the list of attendees
+    attendees.sort()
+
+    // Print the sorted list of attendees
+    println("The following people have attended (sorted):")
+    for (attendee in attendees) {
+        println(attendee)
+    }
+}
