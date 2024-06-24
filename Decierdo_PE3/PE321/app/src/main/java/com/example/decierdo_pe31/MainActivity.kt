961620b28492a8f22b9df7badd2b17f59a7a00a6
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-06-24 18:38:01 +0800

    CREATED final iteration of PE 3.2.1

--- Decierdo_PE3/PE321/app/src/main/java/com/example/decierdo_pe31/MainActivity.kt
+++ Decierdo_PE3/PE321/app/src/main/java/com/example/decierdo_pe31/MainActivity.kt
@@ -8,19 +8,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -32,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -64,6 +68,40 @@ class MainActivity : ComponentActivity() {
 // Root composable function. Build UI here
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
+    var billTotal by remember {
+        mutableStateOf<Float?>(0f)
+    }
+
+    var tipPercentage by remember {
+        mutableStateOf<Float?>(0f)
+    }
+
+    var testString by remember {
+        mutableStateOf<String?>(null)
+    }
+
+    var calculatedTotal by remember {
+        mutableStateOf<Float?>(null)
+    }
+
+    var roundUp by remember {
+        mutableStateOf(false)
+    }
+
+    Column(
+       modifier = modifier
+           .padding(20.dp)
+           .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Nondescript Bistro",
+            fontFamily = FontFamily.Serif,
+            fontSize = 36.sp
+        )
+        // Add image here
+    }
     Column(
         modifier = modifier
             .padding(20.dp)
@@ -71,20 +109,39 @@ fun MyApp(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        /*TODO:
-         1. Add a drop-down menu for the list of server names(Optional)
-         2, Add a text field for order cost - DONE
-         3. 3 radio buttons in a row for different tipping % - DONE
-         4. Slider button to round up total cost - DONE
-         4.1. Make UI Adjustments
-         5. Implement program logic
-         6. Make UI adjustments
-        * */
-        BillTotal()
-        BillField()
-        RadioRowField()
-        RoundUp()
-        CalculateTip()
+        calculatedTotal?.let {
+            BillTotal(
+                totalBill = "%.2f".format(it)
+            )
+        }
+        Spacer(modifier = modifier.height(20.dp))
+        BillField(
+            onValueChange = {
+                newTotal -> billTotal = newTotal
+            },
+            onTestString = {
+                newTotalString -> testString = newTotalString     // For debugging purposes
+            }
+        )
+        Spacer(modifier = modifier.height(20.dp))
+        RadioRowField(
+            onPercentageSelected = {
+                percentage -> tipPercentage = percentage
+            }
+        )
+        RoundUp(
+            onRoundUpChange = {
+                roundUp = it
+            }
+        )
+        CalculateTip(
+            billInitial = billTotal,
+            percentTip = tipPercentage,
+            roundUp = roundUp,
+            onCalculate = {
+                total -> calculatedTotal = total
+            }
+        )
     }
 }
 
@@ -100,10 +157,23 @@ fun BillTotal(modifier: Modifier = Modifier, totalBill: String = "0.00") {
     )
 }
 
+// function: Calculate Tip
+// arguments: billInitial(float), percentTip(float), roundUp(Boolean)
+// returns: billTotal
 @Composable
-fun CalculateTip() {
-    //var onClick: () -> Unit // return a null function for now
-    OutlinedButton(onClick = { /*TODO*/ }) {
+fun CalculateTip(billInitial: Float?, percentTip: Float?, roundUp: Boolean, onCalculate: (Float) -> Unit) {
+    OutlinedButton(
+        onClick = {
+            val billTotal = billInitial ?: 0f           // Null safety stuff
+            val tipPercentage = percentTip ?: 0f
+            var total = billTotal + (billTotal * tipPercentage)
+            if (roundUp) {
+                total = kotlin.math.ceil(total)
+            }
+            onCalculate(total)
+        },
+        modifier = Modifier.padding(16.dp)
+    ) {
         Text(
             text = "Calculate Tip!",
             fontFamily = FontFamily.Serif,
@@ -112,8 +182,13 @@ fun CalculateTip() {
     }
 }
 
+
+
 @Composable
-fun RoundUp(modifier: Modifier = Modifier) {
+fun RoundUp(
+    modifier: Modifier = Modifier,
+    onRoundUpChange: (Boolean) -> Unit
+) {
     var toggled by remember {
         mutableStateOf(false)
     }
@@ -128,7 +203,16 @@ fun RoundUp(modifier: Modifier = Modifier) {
         ) {
             Switch(
                 checked = toggled,
-                onCheckedChange = {toggled = it}
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = Color.White,
+                    checkedTrackColor = Color.Black,
+                    uncheckedThumbColor = Color.Black,
+                    uncheckedTrackColor = Color.Gray
+                ),
+                onCheckedChange = {
+                    toggled = it
+                    onRoundUpChange(it)
+                }
             )
             Spacer(modifier = modifier.width(8.dp))
             Text(
@@ -137,17 +221,24 @@ fun RoundUp(modifier: Modifier = Modifier) {
             )
         }
     }
-
 }
 
+
+
+
 @Composable
-fun RadioRowField(modifier: Modifier = Modifier) {
+fun RadioRowField(
+    modifier: Modifier = Modifier,
+    onPercentageSelected: (Float) -> Unit
+) {
     val percentages = arrayOf("10%", "15%", "20%")
     var selectedPercentage by remember { mutableStateOf("") }
+
     Column {
         Text(
             text = "Select tip",
-            fontFamily = FontFamily.Serif
+            fontFamily = FontFamily.Serif,
+            color = Color.Black // Ensure the text color is visible
         )
         Row(
             modifier = modifier
@@ -159,12 +250,22 @@ fun RadioRowField(modifier: Modifier = Modifier) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     RadioButton(
                         selected = (item == selectedPercentage),
-                        onClick = { selectedPercentage = item },
+                        onClick = {
+                            selectedPercentage = item
+                            val percentageValue = item.removeSuffix("%").toFloat() / 100
+                            onPercentageSelected(percentageValue)
+                        },
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = Color.Black,
+                            unselectedColor = Color.Gray,
+                            disabledSelectedColor = Color.LightGray
+                        )
                     )
                     Text(
                         text = item,
                         fontSize = 20.sp,
-                        fontFamily = FontFamily.Serif
+                        fontFamily = FontFamily.Serif,
+                        color = Color.Black // Ensure the text color is visible
                     )
                     Spacer(modifier = Modifier.width(8.dp)) // Add spacing between each radio button and text pair
                 }
@@ -175,12 +276,25 @@ fun RadioRowField(modifier: Modifier = Modifier) {
 
 
 @Composable
-fun BillField(modifier: Modifier = Modifier) {
+fun BillField(
+    modifier: Modifier = Modifier,
+    onValueChange: (Float?) -> Unit = {},   // Passing empty high-order functions
+    onTestString: (String?) -> Unit = {}
+) {
     var text by remember { mutableStateOf("") } // Variable to be used in the text field composable
 
     OutlinedTextField(
         value = text,
-        onValueChange = { text = it },
+        onValueChange = {
+            text = it
+            val floatValue = it.toFloatOrNull()
+            onValueChange(floatValue)
+            onTestString(text)
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number
+        ),
         label = {
             Text(
                 "Enter bill total",
--- Decierdo_PE3/PE321/app/src/main/java/com/example/decierdo_pe31/ui/theme/Theme.kt
+++ Decierdo_PE3/PE321/app/src/main/java/com/example/decierdo_pe31/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.decierdo_pe31.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -13,33 +12,26 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = woodVeneerBrown,
+    primary = Color(0xFFDFD0B1),
     onPrimary = Color.Black,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    secondary = Color(0xFFCCC2DC),
+    tertiary = Color(0xFFEFB8C8),
+    background = Color.Black,
+    surface = Color.DarkGray
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = woodVeneerBrown,
+    primary = Color(0xFFDFD0B1),
     onPrimary = Color.Black,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = Color(0xFF625b71),
+    tertiary = Color(0xFF7D5260),
+    background = Color.White,
+    surface = Color.LightGray
 )
 
 @Composable
 fun Decierdo_PE31Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -48,7 +40,6 @@ fun Decierdo_PE31Theme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
