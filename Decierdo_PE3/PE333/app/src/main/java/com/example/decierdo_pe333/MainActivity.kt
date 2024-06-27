2e1479ac7c9ff7d041339b83d25615c2ba7a3b74
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-06-27 09:01:13 +0800

    Configured build.gradle & toml library files for ConstraintLayout

---
+++ Decierdo_PE3/PE333/app/src/main/java/com/example/decierdo_pe333/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.decierdo_pe333
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.decierdo_pe333.ui.theme.Decierdo_PE333Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Decierdo_PE333Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Decierdo_PE333Theme {
+        Greeting("Android")
+    }
+}
