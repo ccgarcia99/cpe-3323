bfdf9a3a38e5cdda20fd560352ec3945acf4df91
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-06-30 19:30:48 +0800

    FINISHED basic ConstraintLayout practice. Fuck this framework man

--- views-exercise/views-demo/app/src/main/java/com/example/viewsdemo/MainActivity.kt
+++ views-exercise/views-demo/app/src/main/java/com/example/viewsdemo/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.viewsdemo
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,40 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // constants
+        var tipMultiplier = 0f
+
+        // input
+        val btnCalculateTip = findViewById<Button>(R.id.btnCalculateTip)
+        val etEnterBill = findViewById<EditText>(R.id.etBillField)
+        val radioSelect = findViewById<RadioGroup>(R.id.tipGroup)
+
+        // output
+        val tvTotal = findViewById<TextView>(R.id.tvTotal)
+        val tvTipCost = findViewById<TextView>(R.id.tvTipCostOut)
+
+        radioSelect.setOnCheckedChangeListener{ _, checkedId ->
+            tipMultiplier = when (checkedId) {
+                R.id.rdGood -> 0.10f
+                R.id.rdGreat -> 0.15f
+                R.id.rdAmazing -> 0.25f
+                else -> 0f
+            }
+        }
+
+        btnCalculateTip.setOnClickListener{
+            if(etEnterBill.text.isEmpty()) {
+                "PHP 0.00".also { tvTotal.text = it }
+                "PHP 0.00".also {tvTipCost.text = it}
+                return@setOnClickListener
+            }
+            val baseAmount = etEnterBill.text.toString().toDouble() // parse string into double
+            val tipAmount = baseAmount * tipMultiplier
+            val totalAmount = baseAmount + tipAmount
+            "PHP %.2f".format(totalAmount).also { tvTotal.text = it }
+            "PHP %.2f".format(tipAmount).also { tvTipCost.text= it }
+        }
+
     }
 }
