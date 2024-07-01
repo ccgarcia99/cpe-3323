21d8b5570d932b4f994ae30b8b0766f95076852a
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-07-01 20:57:54 +0800

    Implemented passing data from one activity to another

--- views-exercise/mvc-practice/app/src/main/java/com/example/mvc_practice/MainActivity.kt
+++ views-exercise/mvc-practice/app/src/main/java/com/example/mvc_practice/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.mvc_practice
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mvc_practice.R.id.btnViewProfile
+
+// Logcat tags
+const val TAG = "onToFrags"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +24,49 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // text inputs
+        val etName = findViewById<EditText>(R.id.etName)
+        val etAge = findViewById<EditText>(R.id.etAge)
+        val etAddress = findViewById<EditText>(R.id.etAddress)
+        val etOccupation = findViewById<EditText>(R.id.etOccupation)
+
+        Intent(this, ProfileActivity::class.java).also{
+            it.putExtra("EXTRA_NAME", "N/A")
+            it.putExtra("EXTRA_AGE", "N/A")
+            it.putExtra("EXTRA_ADDR", "N/A")
+            it.putExtra("EXTRA_OCCU", "N/A")
+        }
+
+        val sendToProfile = findViewById<Button>(R.id.btnApply)
+        sendToProfile.setOnClickListener{
+            if (etName.text.isEmpty() || etAge.text.isEmpty() || etAddress.text.isEmpty()
+                || etOccupation.text.isEmpty()) {
+                return@setOnClickListener
+            }
+            Intent(this, ProfileActivity::class.java).also{
+                it.putExtra("EXTRA_NAME", etName.text.toString())
+                it.putExtra("EXTRA_AGE", etAge.text.toString().toInt())
+                it.putExtra("EXTRA_ADDR", etAddress.text.toString())
+                it.putExtra("EXTRA_OCCU", etOccupation.text.toString())
+                startActivity(it)
+            }
+        }
+
         val btnViewProfile = findViewById<Button>(btnViewProfile)
         btnViewProfile.setOnClickListener{
             // Creating an intent
             Intent(this, ProfileActivity::class.java).also {
                 startActivity(it)
             }
+        }
+
+        val btnFragsView = findViewById<Button>(R.id.btnFrags)
+        btnFragsView.setOnClickListener{
+            Intent(this, FragmentsSample::class.java).also {
+                startActivity(it)
+            }
+            Log.i(TAG, "Pressed")
         }
     }
 }
--- views-exercise/mvc-practice/app/src/main/java/com/example/mvc_practice/ProfileActivity.kt
+++ views-exercise/mvc-practice/app/src/main/java/com/example/mvc_practice/ProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.mvc_practice
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,9 +19,26 @@ class ProfileActivity : AppCompatActivity() {
             insets
         }
 
+        val eTvName = intent.getStringExtra("EXTRA_NAME")
+        val eTvAge = intent.getIntExtra("EXTRA_AGE", 0)
+        val eTvAddress = intent.getStringExtra("EXTRA_ADDR")
+        val eTvOccupation = intent.getStringExtra("EXTRA_OCCU")
+
+        val tvName = findViewById<TextView>(R.id.tvName)
+        tvName.text = eTvName
+        val tvAge = findViewById<TextView>(R.id.tvAge)
+        tvAge.text = eTvAge.toString()
+        val tvAddress = findViewById<TextView>(R.id.tvAddress)
+        tvAddress.text = eTvAddress
+        val tvOccupation = findViewById<TextView>(R.id.tvOccupation)
+        tvOccupation.text = eTvOccupation
+
+
+
         val btnGoBack = findViewById<Button>(R.id.btnGoBack)
         btnGoBack.setOnClickListener{
-            finish()    // Kill this activity
+            finish()    // Kill this activity. Values won't persist
         }
+
     }
 }
