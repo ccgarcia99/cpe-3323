4d1fee8e40ee048ed15f26bca9dc739ef1f3cad2
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-07-01 17:14:50 +0800

    IMPLEMENTED basic intents in mvc-exercise

--- views-exercise/mvc-practice/app/src/main/java/com/example/mvc_practice/ProfileActivity.kt
+++ views-exercise/mvc-practice/app/src/main/java/com/example/mvc_practice/ProfileActivity.kt
@@ -1,30 +1,26 @@
 package com.example.mvc_practice
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.mvc_practice.R.id.btnViewProfile
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnViewProfile = findViewById<Button>(btnViewProfile)
-        btnViewProfile.setOnClickListener{
-            // Creating an intent
-            Intent(this, ProfileActivity::class.java).also {
-                startActivity(it)
-            }
+
+        val btnGoBack = findViewById<Button>(R.id.btnGoBack)
+        btnGoBack.setOnClickListener{
+            finish()    // Kill this activity
         }
     }
 }
