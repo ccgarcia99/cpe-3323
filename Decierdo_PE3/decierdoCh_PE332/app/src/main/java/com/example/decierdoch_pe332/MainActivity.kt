f2aabcc722f520b4c713c5f7af7354285904414d
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-07-03 13:07:51 +0800

    Made preset changes on PE331 & created first iteration of PE332 w/ custom themes & typography

---
+++ Decierdo_PE3/decierdoCh_PE332/app/src/main/java/com/example/decierdoch_pe332/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.decierdoch_pe332
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.decierdoch_pe332.ui.theme.DecierdoCh_PE332Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DecierdoCh_PE332Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DecierdoCh_PE332Theme {
+        Greeting("Android")
+    }
+}
