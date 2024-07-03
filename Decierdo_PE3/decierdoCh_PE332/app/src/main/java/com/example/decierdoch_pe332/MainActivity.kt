4baf1c7f906ccad318834ba735aa3ec272e5ae2e
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-07-03 18:47:29 +0800

    Created first iteration of UI & implemented chip group

--- Decierdo_PE3/decierdoCh_PE332/app/src/main/java/com/example/decierdoch_pe332/MainActivity.kt
+++ Decierdo_PE3/decierdoCh_PE332/app/src/main/java/com/example/decierdoch_pe332/MainActivity.kt
@@ -4,14 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -36,7 +53,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun RootComposable(modifier: Modifier = Modifier) {
-    // Access local pre-defined spacings and text sizes
     val spacing = LocalSpacing.current
     val myTextSize = LocalTextSize.current
     DecierdoCh_PE332Theme(
@@ -50,30 +66,78 @@ fun RootComposable(modifier: Modifier = Modifier) {
                 color = MaterialTheme.colorScheme.surface
             ) {
                 ConstraintLayout {
-                    val (labelRef, subHeadingRef) = createRefs()
+                    val (labelRef, subHeadingRef, chipGroup) = createRefs()
                     TopLabel(
                         modifier = modifier
                             .fillMaxWidth()
                             .padding(8.dp)
-                            .constrainAs(labelRef){
+                            .constrainAs(labelRef) {
                                 top.linkTo(parent.top, margin = spacing.marginRegularSpacing)
                                 start.linkTo(parent.start, margin = spacing.marginRegularSpacing)
                                 end.linkTo(parent.end, margin = spacing.marginRegularSpacing)
                             },
                         customTextSize = myTextSize.extraLargeHeaderText
+
                     )
                     SubHeading(
                         modifier = modifier
                             .fillMaxWidth()
                             .padding(8.dp)
-                            .constrainAs(subHeadingRef){
+                            .constrainAs(subHeadingRef) {
                                 top.linkTo(labelRef.bottom, margin = spacing.smallSpacing)
                                 start.linkTo(parent.start, margin = spacing.marginRegularSpacing)
                                 end.linkTo(parent.end, margin = spacing.marginRegularSpacing)
                             },
-                        customTextSize = myTextSize.smallHeaderText)
+                        customTextSize = myTextSize.smallHeaderText
+                    )
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .constrainAs(chipGroup) {
+                                top.linkTo(subHeadingRef.bottom, margin = spacing.smallSpacing)
+                                start.linkTo(parent.start, margin = spacing.marginRegularSpacing)
+                                end.linkTo(parent.end, margin = spacing.marginRegularSpacing)
+                            },
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        ChipOptions(
+                            label = "Burger",
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {}
+                        ChipOptions(
+                            label = "Pasta",
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {}
+                        ChipOptions(
+                            label = "Orange Chicken",
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {}
+                    }
                 }
             }
+        }
+    }
+}
+@Composable
+fun CardListView() {
+    LazyColumn {
+
+    }
+}
+
+@Composable
+fun SideDishCard(modifier: Modifier, imageId: Int) {
+    Box(
+        modifier = modifier
+            .width(320.dp)
+            .height(150.dp),
+    ) {
+        Surface(
+            color = MaterialTheme.colorScheme.secondaryContainer,
+            modifier = modifier.fillMaxSize(),
+            shape = RoundedCornerShape(20.dp)
+        ) {
+
         }
     }
 }
@@ -88,6 +152,7 @@ fun TopLabel(modifier: Modifier, customTextSize: TextUnit) {
         fontSize = customTextSize
     )
 }
+
 @Composable
 fun SubHeading(modifier: Modifier, customTextSize: TextUnit) {
     Text(
@@ -102,11 +167,31 @@ fun SubHeading(modifier: Modifier, customTextSize: TextUnit) {
     )
 }
 
+@Composable
+fun ChipOptions(modifier: Modifier = Modifier, label: String, onClick: () -> Unit) {
+    var isSelected by remember { mutableStateOf(false) }
+    FilterChip(
+        selected = isSelected,
+        onClick = { isSelected = !isSelected },
+        label = { Text(text = label, maxLines = 1) },
+        modifier = modifier,
+        leadingIcon = {
+            if (isSelected) {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = "some icon",
+                    modifier = Modifier.size(FilterChipDefaults.IconSize)
+                )
+            }
+        }
+    )
+}
+
 @Preview(
     showBackground = true,
     showSystemUi = true
 )
 @Composable
-fun GreetingPreview() {
+fun AppPreview() {
     RootComposable()
 }
