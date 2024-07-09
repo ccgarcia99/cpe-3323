bfcb83d5c6c3d0893109c91342440979c4d12e0b
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-07-09 15:58:02 +0800

    Modified list data

--- group-works/le351/app/src/main/java/com/example/amorgandado_enreraPa_decierdoCh_LE351/SoundList.kt
+++ group-works/le351/app/src/main/java/com/example/amorgandado_enreraPa_decierdoCh_LE351/SoundList.kt
@@ -5,21 +5,21 @@ data class SoundItem(val name: String, val soundResId: Int)
 
 data class SoundGroup(val header: String, val imageResId: Int, val sounds: List<SoundItem>)
 // Modify data here
-val weatherSounds = listOf(
+val environmentSounds = listOf(
     SoundItem("Rain", R.raw.rain_1),
     SoundItem("Thunderstorm", R.raw.thunderstorm),
     SoundItem("Wind", R.raw.winds),
     SoundItem("Ocean", R.raw.ocean_1),
-    SoundItem("Birds", R.raw.birds)
+    SoundItem("Sunny Day", R.raw.birds)
 )
 val ringtoneSounds = listOf(
     SoundItem("Classic", R.raw.ringtone_1),
     SoundItem("Beep", R.raw.beep),
     SoundItem("Chime", R.raw.chimes),
-    SoundItem("Alarm", R.raw.siren),
+    SoundItem("Siren", R.raw.siren),
     SoundItem("Bell", R.raw.bell)
 )
 val groups = listOf(
-    SoundGroup("Weather Sounds", R.drawable._681495279040607, weatherSounds),
-    SoundGroup("Ringtone Sounds", R.drawable._708470532817406, ringtoneSounds)
+    SoundGroup("Environments", R.drawable._681495279040607, environmentSounds),
+    SoundGroup("Ringtones", R.drawable._708470532817406, ringtoneSounds)
 )
