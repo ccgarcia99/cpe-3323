11212491e2c26172c30ae52819c3382932c79fe0
Christian Clyde Decierdo <decierdocc1999@gmail.com>
2024-07-09 16:22:21 +0800

    Modified implementation of color picker

--- Demos/color-picker/app/src/main/java/com/example/colorpicker/MainActivity.kt
+++ Demos/color-picker/app/src/main/java/com/example/colorpicker/MainActivity.kt
@@ -9,36 +9,50 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.layoutId
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.example.colorpicker.ui.theme.ColorpickerTheme
 import com.github.skydoves.colorpicker.compose.ColorEnvelope
-import com.github.skydoves.colorpicker.compose.ColorPickerController
 import com.github.skydoves.colorpicker.compose.HsvColorPicker
 import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ColorpickerTheme{
+                val colorViewModel: ColorViewModel = viewModel()
+                MainScreen(colorViewModel = colorViewModel)
+            }
+        }
+    }
+}
 
 // ViewModel to hold selected color
 class ColorViewModel : ViewModel() {
@@ -50,61 +64,12 @@ class ColorViewModel : ViewModel() {
     }
 }
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            val navController = rememberNavController()
-            val colorController = rememberColorPickerController()
-            val colorViewModel: ColorViewModel = viewModel()
-
-            NavHost(navController = navController, startDestination = "MainScreen") {
-                composable(route = "MainScreen") {
-                    MainScreen(navController = navController, colorViewModel = colorViewModel)
-                }
-                composable(route = "ColorPickerScreen") {
-                    ColorPickerScreen(
-                        colorPicker = colorController,
-                        colorViewModel = colorViewModel
-                    )
-                }
-            }
-        }
-    }
-}
-
-// Color picker screen
-@Composable
-fun ColorPickerScreen(
-    colorPicker: ColorPickerController,
-    colorViewModel: ColorViewModel,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        HsvColorPicker(
-            modifier = modifier
-                .fillMaxWidth(0.8f)
-                .height(450.dp)
-                .width(10.dp),
-            controller = colorPicker,
-            onColorChanged = { colorEnvelope: ColorEnvelope ->
-                colorViewModel.updateColor(colorEnvelope.color)
-            }
-        )
-    }
-}
-
-// Main screen
 @Composable
 fun MainScreen(
-    navController: NavController,
     colorViewModel: ColorViewModel,
     modifier: Modifier = Modifier
 ) {
+    val showDialog = remember { mutableStateOf(false) }
     val selectedColor by colorViewModel.selectedColor.collectAsState()
 
     val constraints = ConstraintSet {
@@ -135,13 +100,20 @@ fun MainScreen(
         modifier = modifier.fillMaxSize()
     ) {
         ColorPickerPrompt(
-            navController = navController,
+            showDialog = showDialog,
             modifier = Modifier.layoutId("button")
         )
         ColorBox(
             color = selectedColor,
             modifier = Modifier.layoutId("colorBox")
         )
+
+        if (showDialog.value) {
+            ColorPickerDialog(
+                showDialog = showDialog,
+                colorViewModel = colorViewModel
+            )
+        }
     }
 }
 
@@ -164,15 +136,59 @@ fun ColorBox(color: Color, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ColorPickerPrompt(navController: NavController, modifier: Modifier = Modifier) {
+fun ColorPickerPrompt(showDialog: MutableState<Boolean>, modifier: Modifier = Modifier) {
     ElevatedButton(
         modifier = modifier,
         onClick = {
-            navController.navigate("ColorPickerScreen")
+            showDialog.value = true
         }
     ) {
         Text(text = "Pick a color!")
     }
+}
+
+@Composable
+fun ColorPickerDialog(
+    showDialog: MutableState<Boolean>,
+    colorViewModel: ColorViewModel,
+) {
+    val colorController = rememberColorPickerController()
+    AlertDialog(
+        onDismissRequest = { showDialog.value = false },
+        confirmButton = {
+            TextButton(onClick = { showDialog.value = false }) {
+                Text("Done")
+            }
+        },
+        title = {
+            Text(text = "Pick a Color")
+        },
+        text = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                HsvColorPicker(
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(450.dp),
+                    controller = colorController,
+                    onColorChanged = { colorEnvelope: ColorEnvelope ->
+                        colorViewModel.updateColor(colorEnvelope.color)
+                    }
+                )
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { showDialog.value = false }) {
+                Text("Cancel")
+            }
+        },
+        modifier = Modifier.padding(16.dp) // Ensure proper padding around the dialog
+    )
 }
 
 @Preview(
@@ -181,10 +197,8 @@ fun ColorPickerPrompt(navController: NavController, modifier: Modifier = Modifie
 )
 @Composable
 fun ScreenPreview(modifier: Modifier = Modifier) {
-    val mockNavController = rememberNavController()
     val colorViewModel: ColorViewModel = viewModel()
     MainScreen(
-        navController = mockNavController,
         colorViewModel = colorViewModel
     )
 }
